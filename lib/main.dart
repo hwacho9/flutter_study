208b204f7a1ad47652a5727c3517000b85c24923
hwacho9 <csh77776@gmail.com>
2024-03-29 23:55:28 +0900

    class

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,30 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.red, Colors.blue],
-              begin: Alignment.topLeft,
-            ),
-          ),
-          child: Center(
-            child: Text(
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-                "hi"),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.red, Colors.blue],
+          begin: Alignment.topLeft,
+        ),
+      ),
+      child: Center(
+        child: Text(
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+            "hi"),
+      ),
+    );
+  }
 }
